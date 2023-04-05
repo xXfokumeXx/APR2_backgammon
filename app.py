437015e237f902821862bed21fd7485e39a9d18d
Sasha <sodyl.ssha@gmail.com>
2023-04-05 16:55:48 +0200

    Started making the skeleton of the project

--- app.py
+++
@@ -1,6 +0,0 @@
-print("Czešč")  # this function is printing "Czešč"
-def f(z):
-    return z
-
-def f(g):
-    return g
--- funkce.py
+++
@@ -1,5 +0,0 @@
-def g(y):
-    return 2*y
-
-def ahoj():
-    print("pozdrav z hodiny apr2")
