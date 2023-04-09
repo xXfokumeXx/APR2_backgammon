6790d62cb4a5a0c72e99e9a49a675cf6894cc319
Sasha <sodyl.ssha@gmail.com>
2023-04-09 16:58:54 +0200

    Made background and center border

--- App.py
+++ App.py
@@ -7,8 +7,13 @@ class App(tk.Tk):
         super().__init__()
 
         self.title("Backgammon")
+        self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.resizable(False, False)
 
         self.canvas = tk.Canvas(self, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
         self.canvas.pack()
 
+        self.background = self.canvas.create_rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, fill="#807880", width=0)
+        self.centerBorder = self.canvas.create_rectangle(WINDOW_WIDTH / 2 - WINDOW_WIDTH / 30, 0,
+                                                         WINDOW_WIDTH / 2 + WINDOW_WIDTH / 30,
+                                                         WINDOW_HEIGHT, fill="#302b30")
