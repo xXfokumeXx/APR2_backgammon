d9a31a70de5088414b772ec729b16ad381faa316
xXfokumeXx <radek.cerny847@gmail.com>
2023-04-09 16:25:45 +0200

    Added game_parts folder where should be saved logic parts of the game divided into classes, works like a package

---
+++ game_parts/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+# konstant, ktere jsou nem2nn0 a budeme je pouzivat v dalsich castech programu
+BLACK = (219,140,44)
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+FRAMES_PER_SECOND = 60
+
+ROW, COL = 2, 14  # standartní
+SQUARE_SIZE = SCREEN_WIDTH//ROW  # Velikost jednoho pole pro hrací kámen
